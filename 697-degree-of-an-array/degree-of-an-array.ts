0d396acba44bf91e09eb097654195af7d3ff642b
hanssav <63248102+hanssav@users.noreply.github.com>
2025-12-31 17:02:09 +0700

    Time: 6 ms (98.31%) | Memory: 60.4 MB (77.97%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.ts
+++ 697-degree-of-an-array/degree-of-an-array.ts
@@ -1,31 +1,68 @@
 function findShortestSubArray(nums: number[]): number {
-    const degree = new Map(); 
+    // Kita simpan 3 informasi dalam satu objek agar tidak banyak buat Map/Array
+    // num -> {count, first, last}
+    const stats: Record<number, {c: number, f: number, l: number}> = {};
+    let maxDegree = 0;
 
-    let max_curr = 0; 
-
-    nums.forEach((val, idx) => {
-        if(!degree.has(val)) degree.set(val, []);
-
-        degree.get(val).push(idx)
-        
-        const len = degree.get(val).length
-
-        if(len > max_curr) {
-            max_curr = len
+    for (let i = 0; i < nums.length; i++) {
+        const x = nums[i];
+        if (stats[x] === undefined) {
+            stats[x] = { c: 1, f: i, l: i };
+        } else {
+            stats[x].c++;
+            stats[x].l = i;
         }
-    })
 
-    let res = Infinity;
+        // Update degree tertinggi saat itu juga
+        if (stats[x].c > maxDegree) {
+            maxDegree = stats[x].c;
+        }
+    }
 
-    for(let [num, indices] of degree){
-        if(indices.length === max_curr){
-            const curr_len = indices[indices.length - 1] - indices[0] + 1; 
+    let minLen = Infinity;
 
-            if(curr_len < res){
-                res = curr_len; 
+    // Loop hanya pada kunci yang ada di objek stats
+    for (const x in stats) {
+        const s = stats[x];
+        if (s.c === maxDegree) {
+            const currentLen = s.l - s.f + 1;
+            if (currentLen < minLen) {
+                minLen = currentLen;
             }
         }
     }
 
-    return res
-};
+    return minLen;
+}
+
+// function findShortestSubArray(nums: number[]): number {
+//     const degree = new Map(); 
+
+//     let max_curr = 0; 
+
+//     nums.forEach((val, idx) => {
+//         if(!degree.has(val)) degree.set(val, []);
+
+//         degree.get(val).push(idx)
+        
+//         const len = degree.get(val).length
+
+//         if(len > max_curr) {
+//             max_curr = len
+//         }
+//     })
+
+//     let res = Infinity;
+
+//     for(let [num, indices] of degree){
+//         if(indices.length === max_curr){
+//             const curr_len = indices[indices.length - 1] - indices[0] + 1; 
+
+//             if(curr_len < res){
+//                 res = curr_len; 
+//             }
+//         }
+//     }
+
+//     return res
+// };
