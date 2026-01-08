b1e22503def47b52186cf44fcae52ecac1e787d1
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-08 14:34:10 +0700

    Time: 2 ms (52.50%) | Memory: 57.8 MB (67.50%) - LeetSync

--- 1651-shuffle-string/shuffle-string.ts
+++ 1651-shuffle-string/shuffle-string.ts
@@ -1,12 +1,23 @@
-function restoreString(s: string, indices: number[]): string {
-    const res = ""
-    const len = s.length
+// function restoreString(s: string, indices: number[]): string {
+//     const res = ""
+//     const len = s.length
 
-    const pair = []
+//     const pair = []
+
+//     for(let i = 0; i < len; i++){
+//         pair.push([indices[i], s[i]]); 
+//     }
+
+//     return pair.sort((a,b) => a[0] - b[0]).map(item => item[1]).join("")
+// };
+
+function restoreString(s: string, indices: number[]): string {
+    const len = s.length; 
+    const res = new Array(len); 
 
     for(let i = 0; i < len; i++){
-        pair.push([indices[i], s[i]]); 
+        res[indices[i]] = s[i]
     }
 
-    return pair.sort((a,b) => a[0] - b[0]).map(item => item[1]).join("")
-};
+    return res.join("")
+}
