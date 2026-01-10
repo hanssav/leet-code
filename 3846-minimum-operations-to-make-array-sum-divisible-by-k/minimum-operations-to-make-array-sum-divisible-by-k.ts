b9cabf62432eaa4d1574d17f1fbf566b5ec512c8
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-10 10:34:55 +0700

    Time: 2 ms (48.10%) | Memory: 58.5 MB (64.23%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.ts
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.ts
@@ -1,8 +1,12 @@
 function minOperations(nums: number[], k: number): number {
     // let total = nums.reduce((acc, curr) => acc + curr, 0); 
     let total = 0; 
-    for(let num of nums){
-        total += num
+    // for(let num of nums){
+    //     total += num
+    // }
+
+    for(let i = 0; i < nums.length; i++){
+        total += nums[i]
     }
 
     const need = total % k; 
