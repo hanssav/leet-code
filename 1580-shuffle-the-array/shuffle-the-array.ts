f57f9dbb8ad00b18762f457bdb13eff746b3e6aa
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-11 11:03:16 +0700

    Time: 48 ms (80.15%) | Memory: 58.8 MB (39.46%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.ts
+++ 1580-shuffle-the-array/shuffle-the-array.ts
@@ -1,10 +1,22 @@
 function shuffle(nums: number[], n: number): number[] {
-    const y = nums.splice(n, n); 
-    const x = nums.splice(0, n)
+    // const y = nums.splice(n, n); 
+    // const x = nums.splice(0, n)
 
-    for(let i = 0; i < x.length; i++){
-        nums.push(x[i])
-        nums.push(y[i])
+    // for(let i = 0; i < x.length; i++){
+    //     nums.push(x[i])
+    //     nums.push(y[i])
+    // }
+    // return nums
+
+    const res: number[] = []; 
+    let left = 0; 
+    let right = n; 
+
+    while(left < n){
+        res.push(nums[left], nums[right]); 
+        left++; 
+        right++;
     }
-    return nums
+
+    return res;
 };
