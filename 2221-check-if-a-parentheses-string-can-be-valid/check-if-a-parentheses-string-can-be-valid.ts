cc517744e10e7568faf3d20c95698d5b7a4b0810
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-18 21:03:30 +0700

    Time: 11 ms (66.67%) | Memory: 63.9 MB (100.00%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.ts
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.ts
@@ -1,27 +1,52 @@
 function canBeValid(s: string, locked: string): boolean {
     const n = s.length;
     if (n % 2 !== 0) return false;
-    
-    let left = 0; 
-    let right = n - 1;
 
-    let balance = 0;
-    while(left < s.length){
-        if(locked[left] === "0" || s[left] === "(") balance++;
-        else balance--; 
+    let balanceForward = 0; // Untuk scan kiri -> kanan
+    let balanceBackward = 0; // Untuk scan kanan -> kiri
 
-        if(balance < 0) return false;
-        left++;
+    for (let i = 0; i < n; i++) {
+        // Pointer Maju (dari kiri)
+        if (locked[i] === "0" || s[i] === "(") balanceForward++;
+        else balanceForward--;
+
+        // Pointer Mundur (dari kanan)
+        // Indeks mundur adalah n - 1 - i
+        let j = n - 1 - i;
+        if (locked[j] === "0" || s[j] === ")") balanceBackward++;
+        else balanceBackward--;
+
+        // Jika salah satu balance negatif di titik mana pun, langsung gagal
+        if (balanceForward < 0 || balanceBackward < 0) return false;
     }
 
-    balance = 0;
-    while(right >= 0){
-        if(locked[right] === "0" || s[right] === ")") balance++; 
-        else balance--;
+    return true;
+}
 
-        if(balance < 0) return false;
-        right--;
-    }
+// function canBeValid(s: string, locked: string): boolean {
+//     const n = s.length;
+//     if (n % 2 !== 0) return false;
 
-    return true
-};
+//     let left = 0; 
+//     let right = n - 1;
+
+//     let balance = 0;
+//     while(left < s.length){
+//         if(locked[left] === "0" || s[left] === "(") balance++;
+//         else balance--; 
+
+//         if(balance < 0) return false;
+//         left++;
+//     }
+
+//     balance = 0;
+//     while(right >= 0){
+//         if(locked[right] === "0" || s[right] === ")") balance++; 
+//         else balance--;
+
+//         if(balance < 0) return false;
+//         right--;
+//     }
+
+//     return true
+// };
