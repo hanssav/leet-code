84519edb4480690a56f407229354c76aac3a3a1d
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-19 13:24:15 +0700

    Time: 3 ms (96.49%) | Memory: 56.8 MB (76.32%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.ts
+++ 657-robot-return-to-origin/robot-return-to-origin.ts
@@ -1,17 +1,35 @@
 function judgeCircle(moves: string): boolean {
-    const nav = {
-        "U": [-1, 0], 
-        "D": [1, 0], 
-        "R": [0, 1], 
-        "L": [0, -1],
-    }  
+    let x = 0;
+    let y = 0;
 
-    let curr = [0, 0];
-
-    for(let move of moves){
-        curr[0] += nav[move][0]
-        curr[1] += nav[move][1]
+    for (let i = 0; i < moves.length; i++) {
+        const move = moves[i];
+        
+        // Menggunakan if atau switch jauh lebih cepat daripada akses objek
+        if (move === 'U') y++;
+        else if (move === 'D') y--;
+        else if (move === 'R') x++;
+        else if (move === 'L') x--;
     }
 
-    return curr[0] === 0 && curr[1] === 0 ? true : false;
-};
+    // Kembali ke awal jika posisi akhir adalah (0,0)
+    return x === 0 && y === 0;
+}
+
+// function judgeCircle(moves: string): boolean {
+//     const nav = {
+//         "U": [-1, 0], 
+//         "D": [1, 0], 
+//         "R": [0, 1], 
+//         "L": [0, -1],
+//     }  
+
+//     let curr = [0, 0];
+
+//     for(let move of moves){
+//         curr[0] += nav[move][0]
+//         curr[1] += nav[move][1]
+//     }
+
+//     return curr[0] === 0 && curr[1] === 0 ? true : false;
+// };
