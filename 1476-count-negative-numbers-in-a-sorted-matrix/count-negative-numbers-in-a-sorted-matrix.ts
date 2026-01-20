840d226e24c2e3f8eea67871f2bbe5cf5b7027a4
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-20 23:55:30 +0700

    Time: 0 ms (100.00%) | Memory: 56.4 MB (72.67%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.ts
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.ts
@@ -1,11 +1,21 @@
 function countNegatives(grid: number[][]): number {
     let count = 0; 
 
-    for(const g of grid){
-        for(const c of g){
-            if(c < 0) count++
+    const rows = grid.length; 
+    const cols = grid[0].length; 
+
+    let r = rows - 1; 
+    let c = 0;
+
+    while(r >= 0 && c <= cols){
+        if (grid[r][c] < 0) {
+            // Jika grid[r][c] negatif, maka semua elemen 
+            // dari kolom c sampai akhir baris r adalah negatif
+            count += (cols - c);
+            r--; // Naik ke baris atas
+        } else {
+            c++; // Geser ke kanan untuk mencari angka negatif
         }
     }
-
     return count;
 };
