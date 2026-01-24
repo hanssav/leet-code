34d3ad9dc68354b2a3d451f312876c7fc4aa7d65
hanssav <63248102+hanssav@users.noreply.github.com>
2026-01-24 14:06:00 +0700

    Time: 24 ms (22.13%) | Memory: 77 MB (19.89%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.ts
+++ 287-find-the-duplicate-number/find-the-duplicate-number.ts
@@ -1,29 +1,29 @@
-// function findDuplicate(nums: number[]): number {
-//     const m = new Set();  
-
-//     for(const num of nums){
-//         if(m.has(num)) return num;
-
-//         m.add(num)
-//     }
-
-//     return -1; 
-// };
-
 function findDuplicate(nums: number[]): number {
-    let left = 0; 
-    let right = nums.length - 1;
+    const m = new Set();  
 
-    while(right > left){
-        const mid = Math.floor((left + right) / 2); 
-        const count = nums.filter((num => num <= mid)).length; 
+    for(const num of nums){
+        if(m.has(num)) return num;
 
-        if (count > mid) {
-            right = mid;
-        } else {
-            left = mid + 1;
-        }
+        m.add(num)
     }
 
-    return left;
-}
+    return -1; 
+};
+
+// function findDuplicate(nums: number[]): number {
+//     let left = 0; 
+//     let right = nums.length - 1;
+
+//     while(right > left){
+//         const mid = Math.floor((left + right) / 2); 
+//         const count = nums.filter((num => num <= mid)).length; 
+
+//         if (count > mid) {
+//             right = mid;
+//         } else {
+//             left = mid + 1;
+//         }
+//     }
+
+//     return left;
+// }
