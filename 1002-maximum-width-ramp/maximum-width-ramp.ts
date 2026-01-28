6cee7318253435a5f2b8527b2e01ba05254fd8f4
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-01-28 17:27:48 +0700

    Time: 9 ms (46.67%) | Memory: 64.2 MB (73.33%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.ts
+++ 1002-maximum-width-ramp/maximum-width-ramp.ts
@@ -1,3 +1,28 @@
+function maxWidthRamp(nums: number[]): number {
+    const n = nums.length;
+    const rightMax = new Array(n);
+    
+    // Precompute max values from right
+    rightMax[n - 1] = nums[n - 1];
+    for (let i = n - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(nums[i], rightMax[i + 1]);
+    }
+    
+    let i = 0, j = 1, max = 0;
+    
+    while (j < n) {
+        if (nums[i] <= rightMax[j]) {
+            max = Math.max(max, j - i);
+            j++;
+        } else {
+            i++;
+            if (i === j) j++;
+        }
+    }
+    
+    return max;
+}
+
 // function maxWidthRamp(nums: number[]): number {
 //     let max = 0;
 //     const n = nums.length;
@@ -19,20 +44,20 @@
 //     return max;
 // };
 
-function maxWidthRamp(nums: number[]): number {
-    const indices: number[] = Array.from({ length: nums.length }, (_, i) => i);
+// function maxWidthRamp(nums: number[]): number {
+//     const indices: number[] = Array.from({ length: nums.length }, (_, i) => i);
     
-    // Sort indices based on their corresponding values
-    indices.sort((a, b) => nums[a] - nums[b] || a - b);
+//     // Sort indices based on their corresponding values
+//     indices.sort((a, b) => nums[a] - nums[b] || a - b);
     
-    let maxWidth = 0;
-    let minIndex = nums.length;
+//     let maxWidth = 0;
+//     let minIndex = nums.length;
     
-    // Find maximum difference where larger index comes after smaller index
-    for (const idx of indices) {
-        maxWidth = Math.max(maxWidth, idx - minIndex);
-        minIndex = Math.min(minIndex, idx);
-    }
+//     // Find maximum difference where larger index comes after smaller index
+//     for (const idx of indices) {
+//         maxWidth = Math.max(maxWidth, idx - minIndex);
+//         minIndex = Math.min(minIndex, idx);
+//     }
     
-    return maxWidth;
-}
+//     return maxWidth;
+// }
