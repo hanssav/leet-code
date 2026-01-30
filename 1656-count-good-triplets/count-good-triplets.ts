77f0af69fd5660c6be51fd1d6160e9969ed15fa4
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-01-30 18:37:53 +0700

    Time: 36 ms (100.00%) | Memory: 56 MB (42.10%) - LeetSync

---
+++ 1656-count-good-triplets/count-good-triplets.ts
@@ -0,0 +1,19 @@
+function countGoodTriplets(arr: number[], a: number, b: number, c: number): number {
+    let count = 0; 
+    let n = arr.length; 
+
+    for(let i = 0; i < n - 2; i++){
+        for(let j = i + 1; j < n - 1; j++){
+            if(Math.abs(arr[i] - arr[j]) > a) continue; 
+
+            for(let k = j + 1; k < n; k++){
+                if(Math.abs(arr[j] - arr[k]) > b) continue;
+                if(Math.abs(arr[i] - arr[k]) > c) continue;
+
+                count++
+            }
+        }
+    }
+
+    return count;
+};
