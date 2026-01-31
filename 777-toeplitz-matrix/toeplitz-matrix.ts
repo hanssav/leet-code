4e2bbb4acff4b3b0556efe4fc6479a7270e3cfea
Handi Irawan <63248102+hanssav@users.noreply.github.com>
2026-01-31 19:27:18 +0700

    Time: 0 ms (100.00%) | Memory: 57.6 MB (64.41%) - LeetSync

---
+++ 777-toeplitz-matrix/toeplitz-matrix.ts
@@ -0,0 +1,11 @@
+function isToeplitzMatrix(matrix: number[][]): boolean {
+    const row = matrix.length; 
+    const col = matrix[0].length;
+    for(let r = 1; r < row; r++){
+        for(let c = 1; c < col; c++){
+            if(matrix[r][c] !== matrix[r - 1][c - 1]) return false;
+        }
+    }
+
+    return true;
+};
